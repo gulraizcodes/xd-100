1cb235b8e8d6695eccd0c1381e2bee2f49e722bb
Gulraiz Khan <42954335+gulraizcodes@users.noreply.github.com>
2022-09-04 08:24:20 -0700

    Add files via upload

---
+++ Assets/js/main.js
@@ -0,0 +1,20 @@
+/*=============== SHOW MENU ===============*/
+const navMenu = document.getElementById('n-menu'),
+      navToggle = document.getElementById('n-toggle'),
+      navClose = document.getElementById('n-close')
+
+/*===== MENU SHOW =====*/
+/* Validate if constant exists */
+if(navToggle){
+    navToggle.addEventListener('click', () =>{
+        navMenu.classList.add('show-menu')
+    })
+}
+
+/*===== MENU HIDDEN =====*/
+/* Validate if constant exists */
+if(navClose){
+    navClose.addEventListener('click', () =>{
+        navMenu.classList.remove('show-menu')
+    })
+}
